732cd586f03311d8f323aea77372565446463e81
Giovanni Intini <giovanni@mikamai.com>
2011-12-29 18:59:00 +0100

    wow this is good

--- app.rb
+++ app.rb
@@ -1,82 +1,89 @@
 require './omegle'
+require 'logger'
+
+class StrangerDisconnected < Exception; end
 
 class Question
-   attr_reader :text, :choices
+  attr_reader :text, :choices
 
-   def initialize(text, choices = nil)
-      @text = text
-      @choices = choices
-   end
+  def initialize(text, choices = nil)
+    @text = text
+    @choices = choices
+  end
+
+  def ordered_choices
+    letters = ('a'..'z').to_a
+    choices.inject([]) do |acc, val|
+      acc << "#{letters[acc.size]}) #{val}"
+    end
+  end
 end
 
 class AskOmegle
-   def ask(question)#, strangerstoask) #il tutto va ripetuto per il numero di volte che vogliamo
-      p "asking" #debug
-      Omegle.start(:host => 'cardassia.omegle.com') do |omegle|
-         omegle.typing #typing e stopped typing per me vanno inseriti nel send della libreria omegle 
-         p question.text #debug
-         omegle.send question.text
-         omegle.stopped_typing
-        
-         letter = "a"
-         question.choices.each do |c|
-            omegle.typing
-            p letter + ") " + c #debug
-            omegle.send letter + ") " + c
-            omegle.stopped_typing
-            letter.next!
-         end
+  attr_reader :logger
 
-         omegle.listen do |event|
-            #puts event.inspect #debug
-            remote_messages = event.collect {|e| e if e.first == "gotMessage"}.compact.collect {|e| e.last}
-           
-            puts remote_messages.inspect #debug
-           
-           
-            #qui va trappata la risposta, bisogna capire se ha risposto una delle lettere possibili, in caso positivo ringraziare e chiudere la conversazione, 
-            #in caso negativo chiedere di rispondere solo con una delle alternative possibili
-            #bisogna anche introdurre un timeout 
-           
-            #input da tastiera da console: usato per il debug
-           
-            message = gets.chomp
-            #puts message
+  def initialize(severity = Logger::INFO)
+    @logger = Logger.new(STDOUT)
+    @logger.level = severity
+  end
 
-            if message
-               omegle.typing
-               omegle.send message
-               omegle.stopped_typing
-            end
+  # Metodo principale, chiede e salva la domanda
+  def ask(question)
+    logger.debug "connecting to omegle"
+    Omegle.start(:host => 'cardassia.omegle.com') do |omegle|
 
-            remote_messages = ''
-            message = false
+      omegle.typing
+      logger.info question.text
+      omegle.send question.text
+      omegle.stopped_typing
 
-         end
+      question.ordered_choices.each do |c|
+        omegle.typing
+        logger.info c
+        omegle.send c
+        omegle.stopped_typing
       end
 
-      #end_conversation
+      omegle.listen do |event|
+        logger.debug event.inspect
+        raise StrangerDisconnected.new if event.include? ["strangerDisconnected"]
+        remote_messages = event.collect do |e|
+          e if e.first == "gotMessage"
+        end.compact.collect {|e| e.last}
 
-   end
+        logger.debug remote_messages.inspect
+
+        # qui va trappata la risposta, bisogna capire se ha risposto una delle lettere possibili,
+        # in caso positivo ringraziare e chiudere la conversazione,
+        # in caso negativo chiedere di rispondere solo con una delle alternative possibili
+        # bisogna anche introdurre un timeout
+
+        # input da tastiera da console: usato per il debug
+        message = gets.chomp
+        # puts message
+
+        if message
+          omegle.typing
+          omegle.send message
+          omegle.stopped_typing
+        end
+
+        remote_messages = ''
+        message = false
+      end
+    end
+  end
 end
 
+omegle = AskOmegle.new(Logger::DEBUG)
 
 q = Question.new("I'm a bot, i was born today. I was programmed by a lazy guy that has an ugly job and wants me to do polls in his place, so the question is: which is the worst of 2011", ["justin bieber", "rebecca black", "the twilight movies"])
+omegle.logger.debug "question initialized"
+omegle.logger.debug "question text: #{q.text}"
+q.ordered_choices.each {|c| omegle.logger.debug c}
 
-p "question initialized"
-p "text"
-p q.text
-letter = "a"
-q.choices.each do |c|
-   p letter + ") " + c
-   letter.next!
+begin
+  omegle.ask(q)
+rescue StrangerDisconnected
+  omegle.ask(q)
 end
-
-AskOmegle.new.ask(q)
-
-
-
-
-
-
-
