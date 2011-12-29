d62aea137d7d428d16754718a6e162d58f93ba6d
Giovanni Intini <giovanni@mikamai.com>
2011-12-30 00:37:47 +0100

    loop and save now!

--- Gemfile
+++ Gemfile
@@ -1,1 +1,3 @@
 gem 'json'
+gem 'hiredis'
+gem 'redis', :git => "https://github.com/ezmobius/redis-rb.git" , :require => ["redis", "redis/connection/hiredis"]
--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require './omegle'
 require 'logger'
+require 'redis'
+
+R = Redis.new
 
 class Question
   attr_reader :text, :choices
@@ -11,7 +14,7 @@ class Question
 
   def ordered_choices
     choices.inject([]) do |acc, val|
-      acc << "#{acc.size}) #{val}"
+      acc << "#{acc.size + 1}) #{val}"
     end
   end
 end
@@ -50,8 +53,25 @@ class AskOmegle
         logger.debug remote_messages.inspect
 
         remote_messages.each do |msg|
+          thanks = "Thank you, you have been very helpful. Goodbye!"
+          no_thanks = ["Just the number on a single line, please. Help me :)",
+            "please, just a single number o a single line, I'll be thankful forever",
+            "just one number please?"].sample
+
           logger.info msg
-          return false if msg =~ /\d/
+          if result = msg.strip.match(/^\d$/)
+            omegle.typing
+            omegle.send thanks
+            logger.info thanks
+            omegle.stopped_typing
+            R.zincrby "omegle:results", 1, question.choices[result[0].to_i - 1]
+            return false
+          else
+            omegle.typing
+            omegle.send no_thanks
+            logger.info no_thanks
+            omegle.stopped_typing
+          end
         end
         # qui va trappata la risposta, bisogna capire se ha risposto una delle lettere possibili,
         # in caso positivo ringraziare e chiudere la conversazione,
@@ -72,13 +92,13 @@ class AskOmegle
         message = false
 
         return false if event.include? ["strangerDisconnected"]
-
+        true
       end
     end
   end
 end
 
-omegle = AskOmegle.new
+omegle = AskOmegle.new(Logger::DEBUG)
 
 question_text =<<-EOF
 Hi, I'm a bot, an AI if you will, and I was born today. I was programmed by a lazy guy that has an ugly job, and he programmed me to ask you a simple question: which is the worst of 2011? Please answers just with the number representing your choice, I'm still not very good at understanding humans.
